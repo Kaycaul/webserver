570abc333e21999886b27089d55aa5d54a76ac30
Kaycaul <84094849+Kaycaul@users.noreply.github.com>
2023-10-18 19:40:06 -0400

    pretty responsive wow css variables crazy

--- public/index.js
+++ public/index.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+// wait to finish loading
+document.getElementById("contact").addEventListener("click", contact);
+document.getElementById("comms").addEventListener("click", comms);
+
+function contact() {
+    location.href = "https://doughcatball.carrd.co/#contacts";
+}
+
+function comms() {
+    location.href = "https://doughcatball.carrd.co/#comms";
+}
