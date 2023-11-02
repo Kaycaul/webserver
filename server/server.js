22f111e210dcdbe977f7b2002ee45a9a61ec7ed2
Kaycaul <84094849+Kaycaul@users.noreply.github.com>
2023-11-02 10:41:02 -0400

    comments

--- server/server.js
+++ server/server.js
@@ -4,14 +4,17 @@ const app = express();
 const path = require("path");
 const port = 2763;
 
-app.use(morgan("dev")); // log all requests using morgan
+// middleware to log all requests before doing anything
+app.use(morgan("dev"));
 
+// send the index when requesting the root
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-// Middleware to handle 404 errors for resources in the `../public` folder
+// send anything in the public folder
 app.use(express.static(path.join(__dirname, "../public")));
+// finally, send 404s
 app.use((req, res) => {
 	res.status(404).send("404 Not Found");
 });
