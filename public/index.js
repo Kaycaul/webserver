7522c70216b711db50b8ec30b6fa0496dec9d9c2
Kaycaul <84094849+Kaycaul@users.noreply.github.com>
2024-05-24 19:29:04 -0400

    boopcounter works client side

--- public/index.js
+++ public/index.js
@@ -1,6 +1,7 @@
-// wait to finish loading
+// deferred script
 document.getElementById("contact").addEventListener("click", contact);
 document.getElementById("comms").addEventListener("click", comms);
+document.getElementById("profilepicture").addEventListener("click", boop);
 
 function contact() {
     location.href = "https://doughcatball.carrd.co/#contacts";
@@ -8,4 +9,13 @@ function contact() {
 
 function comms() {
     location.href = "https://doughcatball.carrd.co/#comms";
+}
+
+let boops = 0;
+
+function boop() {
+    boops++;
+    boopCounter = document.getElementById("boop-counter");
+    boopCounter.hidden = false;
+    boopCounter.innerText = "Boops: " + boops;
 }
